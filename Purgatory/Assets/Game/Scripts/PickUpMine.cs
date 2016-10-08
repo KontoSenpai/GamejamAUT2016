49b8dc0201d8cc1e177739d5893f6e7c51f7b3ee
KontoSenpai <senechal.jimmy@gmail.com>
2016-10-07 23:31:36 -0400

    Creation of some Sprites
    -Pickup mine
    -Pickup tower
    
    -Demon mine
    -Angel mine
    
    -Demon tower
    -Angel tower

---
+++ Purgatory/Assets/Game/Scripts/PickUpMine.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpMine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetType() == typeof(BoxCollider2D) && other.tag == "Player")
+        {
+            print("Mine Picked up");
+            Destroy(gameObject);
+        }
+    }
+}
