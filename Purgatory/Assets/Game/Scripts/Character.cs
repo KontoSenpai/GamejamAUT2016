ae03de7c2a30f613cd2d4aadc4d6179296f87e1e
SchambourgMaxime <maxime.schambourg@gmail.com>
2016-10-07 21:26:01 -0400

    ADD : new base class for a character

---
+++ Purgatory/Assets/Game/Scripts/Character.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+    private Vector2 m_position;
+    private Vector2 m_direction;
+
+    private byte m_isDemon;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+
+    public Vector2 getPosition() { return m_position; }
+    public Vector2 getDirection() { return m_isDemon; }
+}
