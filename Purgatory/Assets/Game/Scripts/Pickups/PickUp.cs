021fc4b60c90ad18cb8856e1355961d0f0090637
KontoSenpai <senechal.jimmy@gmail.com>
2016-10-08 00:19:04 -0400

    Creation de scripts de behavior.

---
+++ Purgatory/Assets/Game/Scripts/Pickups/PickUp.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        AddInventory(gameObject);
+        Destroy(gameObject);
+    }
+
+    public void AddInventory(GameObject type)
+    {
+        print("TO DO : Send to user inventory");
+    }
+}
