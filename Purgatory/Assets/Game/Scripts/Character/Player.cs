c5b6022982d8fbf972b332de0e1b94b9d44f50b7
Warren Benyahia <Warren@MBP-de-Warren.local>
2016-10-08 12:40:24 -0400

    ADD : Shooting

--- Purgatory/Assets/Game/Scripts/Character/Player.cs
+++ Purgatory/Assets/Game/Scripts/Character/Player.cs
@@ -3,14 +3,25 @@ using System.Collections;
 
 public class Player : Character {
 
+	public GameObject[] shoot;
+	private GameObject shootingProjectile;
+	private Transform shootSpawn;
+	private Vector3 velocity = Vector3.zero;
+
+	public float fireRate;
+	private float nextFire;
+
 	// Use this for initialization
 	void Start ()
     {
         base.Start();
-        if (gameObject.name.Contains("Angel"))
-            setIsBright(true);
-        else
-            setIsDark(true);
+		if (gameObject.name.Contains ("Demon")) {
+			shootingProjectile = shoot [0];
+			setIsBright (true);
+		} else {
+			shootingProjectile = shoot [1];
+			setIsDark (true);
+		}
 	}
 
     void Update()
@@ -31,7 +42,34 @@ public class Player : Character {
         else if (Input.GetAxis("Vertical") < 0.5 &&
                  Input.GetAxis("Vertical") > -0.5)
             stopVerticalMovement();
+/*
+		if (Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
 
+			aimNShoot(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+		}//if (Input.GetAxis ("HorizontalAiming") != 0 || Input.GetAxis ("VerticalAiming") != 0) {
+		//	aimNShoot(Input.GetAxis ("HorizontalAiming"), Input.GetAxis ("VerticalAiming"));
+		//}
+
+*/
     }	
+
+	//public void aimNShoot(float rightStickX, float rightStickY) {
+	public void aimNShoot(Vector3 mouse) {
+		
+		shootSpawn.position = gameObject.GetComponent<Transform>().position; 
+
+		shootSpawn.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(mouse.x,  mouse.y) 
+			* Mathf.Rad2Deg, transform.eulerAngles.z);
+		
+		shootSpawn.rotation = Quaternion.Euler (shootSpawn.eulerAngles);
+
+		GameObject projectile = (GameObject)Instantiate (shootingProjectile, shootSpawn.position, shootSpawn.rotation);
+
+		velocity = new Vector3 (mouse.x, mouse.y, 0.0f);
+
+		projectile.GetComponent<ShootProjectile> ().setVelocity(velocity);
+
+	}
 
 }
---
+++ Purgatory/Assets/Game/Scripts/ShootProjectile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootProjectile : MonoBehaviour {
+
+	public float speed;
+	private Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Update() {
+
+		transform.position += velocity * speed * Time.deltaTime;
+	}
+
+	public void setVelocity(Vector2 velocity) {
+		this.velocity = velocity;
+	}
+}
