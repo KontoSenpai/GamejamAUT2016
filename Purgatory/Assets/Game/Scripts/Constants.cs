952f5d94ebc6d0ab68bebbb8170f137c500c8833
SchambourgMaxime <maxime.schambourg@gmail.com>
2016-10-07 22:02:25 -0400

    UPDATE : base character has setters and update UPDATE : Constants direction is now UP DOWN LEFT RIGHT

--- Purgatory/Assets/Game/Scripts/Constants.cs
+++ Purgatory/Assets/Game/Scripts/Constants.cs
@@ -5,7 +5,7 @@ public static class Constants {
 
     public const uint UP = 0;
     public const uint DOWN = 1;
-    public const uint RIGHT = 2;
-    public const uint LEFT = 3;
+    public const uint LEFT = 2;
+    public const uint RIGHT = 3;
 
 }
