22d9e4763c8ec2631dc5f257e3719f9376c43b12
Maxime Schambourg <maxime.schambourg@gmail.com>
2016-10-09 01:21:22 -0400

    ADD : Spawners on map EDIT : better grid

--- Purgatory/Assets/Game/Scripts/MapGrid.cs
+++ Purgatory/Assets/Game/Scripts/MapGrid.cs
@@ -11,7 +11,11 @@ public class MapGrid : MonoBehaviour {
     public float yMax;
 
     public GameObject backgroundTile;
-    public GameObject rockTile;
+    public GameObject[] obstacleTile;
+    public GameObject angelBaseTile;
+    public GameObject demonBaseTile;
+    public GameObject wandererSpawnerTile;
+    public GameObject pickUpSpawnerTile;
 
     private uint[][] mapGrid;
     private float cellWidth;
@@ -19,36 +23,11 @@ public class MapGrid : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        
-        cellWidth = (Mathf.Abs(xMin) + Mathf.Abs(xMax)) / column;
-        cellHeight = (Mathf.Abs(yMin) + Mathf.Abs(yMax)) / row;
 
-        mapGrid = new uint[row][];
-
-        for (uint i = 0; i < row; i++)
-        {
-
-            mapGrid[i] = new uint[column];
-
-            for (uint j = 0; j < column; j++)
-            {
-
-                mapGrid[i][j] = Constants.EMPTY;
-
-                backgroundTile.transform.localScale = new Vector3(cellWidth / backgroundTile.GetComponent<SpriteRenderer>().sprite.bounds.size.x * 1.01f,
-                                                                  cellHeight / backgroundTile.GetComponent<SpriteRenderer>().sprite.bounds.size.y * 1.01f,
-                                                                  0);
-                Instantiate(backgroundTile, new Vector3(xMin + j * cellWidth, yMax - i * cellHeight, 0), Quaternion.identity);
-
-
-            }
-        }
-
-        rockTile.transform.localScale = new Vector3(cellWidth / rockTile.GetComponent<SpriteRenderer>().sprite.bounds.size.x * 1.01f,
-                                                    cellHeight / rockTile.GetComponent<SpriteRenderer>().sprite.bounds.size.y * 1.01f,
-                                                    0);
-        Instantiate(rockTile, new Vector3(xMin + 2 * cellWidth, yMax - 3 * cellHeight, 0), Quaternion.identity);
-        mapGrid[3][2] = Constants.OBSTACLE;
+        gridSetup();
+        setObstacles();
+        setPlayerBasesTiles(10, 2, 2, 14);
+        setWandererSpawners();       
 
         //GameObject.FindObjectOfType<Seeker>().seek(new Vector3(-5.0f, -5.0f));
 
@@ -117,6 +96,128 @@ public class MapGrid : MonoBehaviour {
         return new Vector2(xEnd - xStart, yEnd - yStart);
     }
 
+    public void gridSetup()
+    {
+        cellWidth = (Mathf.Abs(xMin) + Mathf.Abs(xMax)) / column;
+        cellHeight = (Mathf.Abs(yMin) + Mathf.Abs(yMax)) / row;
+
+        mapGrid = new uint[row][];
+
+        for (uint i = 0; i < row; i++)
+        {
+
+            mapGrid[i] = new uint[column];
+
+            for (uint j = 0; j < column; j++)
+            {
+
+                mapGrid[i][j] = Constants.EMPTY;
+
+                backgroundTile.transform.localScale = new Vector3(cellWidth / backgroundTile.GetComponent<SpriteRenderer>().sprite.bounds.size.x * 1.01f,
+                                                                  cellHeight / backgroundTile.GetComponent<SpriteRenderer>().sprite.bounds.size.y * 1.01f,
+                                                                  0);
+                Instantiate(backgroundTile, new Vector3(xMin + j * cellWidth, yMax - i * cellHeight, 0), Quaternion.identity);
+
+            }
+        }
+    }
+
+    public void setObstacleTile(uint row, uint column)
+    {
+        GameObject currentObstacleTile = obstacleTile[Random.Range(0, obstacleTile.Length)];
+
+        currentObstacleTile.transform.localScale = new Vector3(cellWidth / currentObstacleTile.GetComponent<SpriteRenderer>().sprite.bounds.size.x,
+                                                               cellHeight / currentObstacleTile.GetComponent<SpriteRenderer>().sprite.bounds.size.y,
+                                                               0);
+        Instantiate(currentObstacleTile, new Vector3(xMin + column * cellWidth, yMax - row * cellHeight, 0), Quaternion.identity);
+        mapGrid[row][column] = Constants.OBSTACLE;
+    }
+
+    public void setObstacles()
+    {
+        setObstacleTile(0, 4);
+        setObstacleTile(1, 12);
+        setObstacleTile(3, 10);
+        setObstacleTile(4, 2);
+        setObstacleTile(4, 9);
+        setObstacleTile(4, 13);
+        setObstacleTile(5, 2);
+        setObstacleTile(5, 3);
+        setObstacleTile(7, 13);
+        setObstacleTile(7, 14);
+        setObstacleTile(8, 3);
+        setObstacleTile(8, 7);
+        setObstacleTile(8, 14);
+        setObstacleTile(9, 6);
+        setObstacleTile(11, 4);
+        setObstacleTile(12, 12);
+    }
+
+    public void setPlayerBasesTiles(uint angelBaseRow, uint angelBaseColumn, uint demonBaseRow, uint demonBaseColumn)
+    {
+        angelBaseTile.transform.localScale = new Vector3(cellWidth / angelBaseTile.GetComponent<SpriteRenderer>().sprite.bounds.size.x,
+                                                         cellHeight / angelBaseTile.GetComponent<SpriteRenderer>().sprite.bounds.size.y,
+                                                          0);
+        Instantiate(angelBaseTile, 
+                    new Vector3(xMin + angelBaseColumn * cellWidth + cellWidth/2, yMax - angelBaseRow * cellHeight - cellHeight/2, 0), 
+                    Quaternion.identity);
+        mapGrid[angelBaseRow][angelBaseColumn] = Constants.CRYSTAL_ANGEL;
 
 
+        demonBaseTile.transform.localScale = new Vector3(cellWidth / demonBaseTile.GetComponent<SpriteRenderer>().sprite.bounds.size.x,
+                                                         cellHeight / demonBaseTile.GetComponent<SpriteRenderer>().sprite.bounds.size.y,
+                                                          0);
+        Instantiate(demonBaseTile,
+                    new Vector3(xMin + demonBaseColumn * cellWidth + cellWidth / 2, yMax - demonBaseRow * cellHeight - cellHeight / 2, 0),
+                    Quaternion.identity);
+        mapGrid[demonBaseRow][demonBaseColumn] = Constants.CRYSTAL_DEMON;
+    }
+
+    public void setWandererSpawnerTile(uint row, uint column)
+    {
+        wandererSpawnerTile.transform.localScale = new Vector3(cellWidth / wandererSpawnerTile.GetComponent<SpriteRenderer>().sprite.bounds.size.x,
+                                                         cellHeight / wandererSpawnerTile.GetComponent<SpriteRenderer>().sprite.bounds.size.y,
+                                                          0);
+        Instantiate(wandererSpawnerTile,
+                    new Vector3(xMin + column * cellWidth + cellWidth / 2, yMax - row * cellHeight - cellHeight / 2, 0),
+                    Quaternion.identity);
+        mapGrid[row][column] = Constants.EMPTY;
+    }
+
+    public void setWandererSpawners()
+    {
+        setWandererSpawnerTile(1, 1);
+        setWandererSpawnerTile(1, 5);
+        setWandererSpawnerTile(1, 10);
+        setWandererSpawnerTile(2, 3);
+        setWandererSpawnerTile(5, 6);
+        setWandererSpawnerTile(7, 10);
+        setWandererSpawnerTile(10, 13);
+        setWandererSpawnerTile(11, 6);
+        setWandererSpawnerTile(11, 11);
+        setWandererSpawnerTile(11, 15);
+    }
+
+    
+    public void setPickUpSpawnerTile(uint row, uint column)
+    {
+        pickUpSpawnerTile.transform.localScale = new Vector3(cellWidth / pickUpSpawnerTile.GetComponent<SpriteRenderer>().sprite.bounds.size.x,
+                                                             cellHeight / pickUpSpawnerTile.GetComponent<SpriteRenderer>().sprite.bounds.size.y,
+                                                             0);
+        Instantiate(pickUpSpawnerTile,
+                    new Vector3(xMin + column * cellWidth + cellWidth / 2, yMax - row * cellHeight - cellHeight / 2, 0),
+                    Quaternion.identity);
+        mapGrid[row][column] = Constants.EMPTY;
+    }
+
+    public void setPickUpSpawners()
+    {
+        setPickUpSpawnerTile(1, 15);
+        setPickUpSpawnerTile(2, 7);
+        setPickUpSpawnerTile(4, 3);
+        setPickUpSpawnerTile(8, 13);
+        setPickUpSpawnerTile(10, 9);
+        setPickUpSpawnerTile(11, 1);
+
+    }
 }
--- Purgatory/Assets/Game/Scripts/Spawn.cs
+++ Purgatory/Assets/Game/Scripts/Spawn.cs
@@ -3,17 +3,43 @@ using System.Collections;
 
 public class Spawn : MonoBehaviour {
 
+    public GameObject wanderer;
 
+    public uint wandererSpawnLimit;
+    public uint pickUpSpawnLimit;
 
-	// Use this for initialization
-	void Start () {
-	
+    public uint wandererSpawnDelay;
+    public uint pickUpSpawnDelay;
 
+    private float timer;
+    private static uint numberOfWanderers;
 
+    // Use this for initialization
+    void Start () {
+
+        timer = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        if (Time.time - timer >= wandererSpawnDelay && numberOfWanderers <= wandererSpawnLimit)
+        {
+            timer = Time.time;
+            SpawnWanderer();
+        }
+
+    }
+
+    public void SpawnPickUp()
+    {
+
+    }
+
+    public void SpawnWanderer()
+    {
+        Instantiate(wanderer, transform.position, Quaternion.identity);
+        numberOfWanderers++;
+    }
+        
 }
