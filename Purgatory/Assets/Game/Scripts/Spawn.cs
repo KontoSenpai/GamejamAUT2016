2d73287291c74ea2bf49c3b1ceb2ee4638ceccf2
Maxime Schambourg <maxime.schambourg@gmail.com>
2016-10-09 04:33:48 -0400

    EDIT : spawning

--- Purgatory/Assets/Game/Scripts/Spawn.cs
+++ Purgatory/Assets/Game/Scripts/Spawn.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Spawn : MonoBehaviour {
 
     public GameObject wanderer;
+    public GameObject[] pickUps;  //0: turret, 1: wall, 2: mine
 
     public uint wandererSpawnLimit;
     public uint pickUpSpawnLimit;
@@ -11,21 +12,29 @@ public class Spawn : MonoBehaviour {
     public uint wandererSpawnDelay;
     public uint pickUpSpawnDelay;
 
-    private float timer;
+    private float timerWanderer;
+    private float timerPickUp;
     private static uint numberOfWanderers;
 
     // Use this for initialization
     void Start () {
 
-        timer = Time.time;
+        timerWanderer = Time.time;
+        timerPickUp = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Time.time - timer >= wandererSpawnDelay && numberOfWanderers <= wandererSpawnLimit)
+        if (CompareTag("WandererSpawner") && Time.time - timerWanderer >= wandererSpawnDelay && numberOfWanderers <= wandererSpawnLimit)
         {
-            timer = Time.time;
+            timerWanderer = Time.time;
+            SpawnWanderer();
+        }
+
+        if (CompareTag("PickUpSpawner") && Time.time - timerPickUp >= pickUpSpawnDelay)
+        {
+            timerPickUp = Time.time;
             SpawnWanderer();
         }
 
@@ -33,6 +42,23 @@ public class Spawn : MonoBehaviour {
 
     public void SpawnPickUp()
     {
+        int rand = Random.Range(0, 10);
+
+        // 0 <= rand < 4    -> 40% turret
+        // 4 <= rand < 8    -> 40% wall
+        // 8 <= rand < 10   -> 20% mine
+        if (rand < 4)       //turret
+        {
+            Instantiate(pickUps[0], transform.position, Quaternion.identity);
+        }
+        else if (rand > 7)  //mine
+        {
+            Instantiate(pickUps[2], transform.position, Quaternion.identity);
+        }
+        else                //wall
+        {
+            Instantiate(pickUps[1], transform.position, Quaternion.identity);
+        }
 
     }
 
